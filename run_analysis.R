19884a1f31b236f32c5d4400b42002636b46ca6b
ewilson294 <ecwil2@yahoo.com>
2021-05-25 16:49:39 -0500

    Delete function Read data from /train

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
-run_analysis <- function(){
-    
-}
+## run_analysis.R
+## Author: Eric Wilson
+## Date Created: 23 May 2021
+
+X_train = read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = "")
+y_train = read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+subject_train = read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
