58709fc7600ba180e0f0ab6e60c7c34b0d09db12
ewilson294 <ecwil2@yahoo.com>
2021-05-25 21:19:35 -0500

    Add reading of data from /test Add reading of feature names Add reading of activity labels

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,19 @@
 ## run_analysis.R
 ## Author: Eric Wilson
 ## Date Created: 23 May 2021
+## Required Libraries: dplyr
 
-X_train = read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = "")
-y_train = read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
-subject_train = read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+library(dplyr)
+
+# Read data from /train
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = "")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+
+# Read data from /test
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE, sep = "")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+
+features <- read.table("UCI HAR Dataset/features.txt")
+activity_labels <-read.table("UCI HAR Dataset/activity_labels.txt")
